cb7d4e8278ded4220a58bb4c1f2520f2e3dad21f
Serin Delaunay <serin-delaunay@gmail.com>
2016-10-23 16:21:12 +0100

    test json maker more

--- tests/testall.py
+++ tests/testall.py
@@ -21,7 +21,7 @@ def correct_format(text):
     obj = json.loads(text)
 
 
-# In[23]:
+# In[26]:
 
 class json_tests(unittest.TestCase):
     def validate_format(self, obj):
@@ -41,24 +41,38 @@ class json_tests(unittest.TestCase):
         empty = make_json([])
         obj = json.loads(empty)
         self.validate_format(obj)
-    #def validate_entry(self, obj):
-    #    pass
+    def validate_entry(self, pronouns, obj):
+        self.assertEqual(pronouns[0], obj['subject'])
+        self.assertEqual(pronouns[1], obj['object'])
+        self.assertEqual(pronouns[2], obj['dependentPossessive'])
+        self.assertEqual(pronouns[3], obj['independentPossessive'])
+        self.assertEqual(pronouns[4], obj['reflexive'])
     def accepts_single(self, pronouns):
         single = make_json([pronouns])
         obj = json.loads(single)
         self.validate_format(obj)
-        self.assertEqual(pronouns[0], obj['thirdPersonPronouns'][0]['subject'])
-        self.assertEqual(pronouns[1], obj['thirdPersonPronouns'][0]['object'])
-        self.assertEqual(pronouns[2], obj['thirdPersonPronouns'][0]['dependentPossessive'])
-        self.assertEqual(pronouns[3], obj['thirdPersonPronouns'][0]['independentPossessive'])
-        self.assertEqual(pronouns[4], obj['thirdPersonPronouns'][0]['reflexive'])
+        self.validate_entry(pronouns, obj['thirdPersonPronouns'][0])
     def test_accepts_single(self):
         self.accepts_single(('they', 'them', 'their', 'theirs', 'themself'))
         self.accepts_single(('moo', 'quack', 'Carmilla Karnstein', 'a'*2048,
                              '¬!"£$%^&*()_+`1234567890-=QWERTYUIOP{}qwertyuiop[]'
                              'ASDFGHJKL:@~asdghjkl;\'#|ZXCVBNM<>?\\zxcvbnm,./'))
-    #def test_accepts_multiple(self):
-    #    pass
+    def test_accepts_multiple(self):
+        multiple = make_json([['a','b','c','d','e'],['f','g','h','i','j']])
+        obj = json.loads(multiple)
+        self.validate_format(obj)
+        self.validate_entry(['a','b','c','d','e'],obj['thirdPersonPronouns'][0])
+        self.validate_entry(['f','g','h','i','j'],obj['thirdPersonPronouns'][1])
+    def test_removes_dupes(self):
+        multiple = make_json([['a','b','c','d','e'],
+                              ['f','g','h','i','j'],
+                              ['a','b','c','d','e']])
+        obj = json.loads(multiple)
+        self.validate_format(obj)
+        self.assertEqual(len(obj['thirdPersonPronouns']), 2)
+        self.validate_entry(['a','b','c','d','e'],obj['thirdPersonPronouns'][0])
+        self.validate_entry(['f','g','h','i','j'],obj['thirdPersonPronouns'][1])
+        
 
 
 # In[ ]:
