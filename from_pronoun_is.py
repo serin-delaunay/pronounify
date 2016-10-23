7962c0e66b75b56fd437457fb1f95ad57898d048
Serin Delaunay <serin-delaunay@gmail.com>
2016-10-23 17:58:41 +0100

    get pronoun.is by http

--- from_pronoun_is.py
+++ from_pronoun_is.py
@@ -3,21 +3,38 @@
 
 # In[1]:
 
-def extract(text):
-    return [s.split() for s in text.split('\n')]
+import urllib.request
 
 
 # In[2]:
 
-def result():
-    f = open('pronoun_is.txt')
-    source = f.read()
-    f.close()
-    # https://github.com/witch-house/pronoun.is/blob/develop/resources/pronouns.tab
-    return extract(source)
+import requests
 
 
 # In[3]:
+
+def extract(text):
+    return [s.split() for s in text.split('\n') if len(s)>0]
+
+
+# In[4]:
+
+def get_content(url):
+    rq = requests.get(url)
+    content = rq.content.decode()
+    rq.close()
+    return content
+
+
+# In[5]:
+
+def result():
+    target = 'https://raw.githubusercontent.com/witch-house/pronoun.is/develop/resources/pronouns.tab'
+    source = get_content(target)
+    return extract(source)
+
+
+# In[6]:
 
 if __name__ == '__main__':
     printme = result()
